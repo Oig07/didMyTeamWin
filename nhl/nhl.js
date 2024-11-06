54f9dfe263b810676d6f3571fd055aad94888cab
Oig07 <giovanni.m2225@gmail.com>
2024-11-05 20:52:59 -0600

    NHL - Added
    Updated the NHL section to have the additional logic to account for games that are "In Progress", at the end of the period/quarter, and when a game is scheduled.

--- nhl/nhl.js
+++ nhl/nhl.js
@@ -121,26 +121,43 @@ async function fetchScoreboard(teamAbbr) {
                 return estTime;
             }
 
-            // Check if Game Status = Scheduled
-        if(data.events[0].status.type.description === "Scheduled"){
-            const scheduleItem = document.createElement('li');
-            const gameDate = parseTimeInEST(data.events[0].date)
-            scheduleItem.textContent = `The ${selectedTeamShortName} are scheduled to play against the ${opponentCompetitor.team.shortDisplayName} today at ${gameDate} EST.`
-            gameList.appendChild(scheduleItem);
-            return;
-        }
-
-            // Determine the game result text
-            let resultText = "";
-            if (selectedTeamScore > opponentScore) {
-                resultText = `The ${selectedTeamCompetitor.team.shortDisplayName} win over the ${opponentCompetitor.team.shortDisplayName}! Final Score: `;
-            } else if (selectedTeamScore < opponentScore) {
-                resultText = `The ${selectedTeamCompetitor.team.shortDisplayName} lose to the ${opponentCompetitor.team.shortDisplayName}! Final Score: `;
-            } else {
-                resultText = `The ${selectedTeamCompetitor.team.shortDisplayName} tie with the ${opponentCompetitor.team.shortDisplayName}! Final Score: `;
+            function getOrdinalPeriod(period){
+                switch(period){
+                    case 1: return "1st";
+                    case 2: return "2nd";
+                    case 3: return "3rd";
+                    case 4: return "4th";
+                    default: return `${period}th`
+                }
             }
 
-            gameItem.textContent = `${resultText} (${selectedTeamScore} - ${opponentScore})`;
+            const gameStatus = game.status.type.description;
+            const gamePeriod = game.status.period;
+            const gameOrdinalPeriod = getOrdinalPeriod(gamePeriod);
+            const gameClock = game.status.displayClock;
+
+            if (gameStatus === "Scheduled") {
+                const gameDate = parseTimeInEST(game.date);
+                gameItem.innerHTML = `The ${selectedTeamShortName} are scheduled to play against the ${opponentCompetitor.team.shortDisplayName} today at ${gameDate} EST.`;
+            } else if (gameStatus === "In Progress") {
+                // Display a message for ongoing games
+                gameItem.innerHTML = `The ${selectedTeamShortName} are currently playing against the ${opponentCompetitor.team.shortDisplayName}. There is ${gameClock} left in the ${gameOrdinalPeriod} Period. <br>Current Score: ${selectedTeamShortName} (${selectedTeamScore}) - ${opponentCompetitor.team.shortDisplayName} (${opponentScore}).`;
+
+            } else if (gameStatus === "End of Period"){
+                // Display a message when games are inbetween periods
+                gameItem.innerHTML = `It is the end of the ${gameOrdinalPeriod} Period. The ${selectedTeamShortName} are currently playing against the ${opponentCompetitor.team.shortDisplayName}. <br>Current Score: ${selectedTeamShortName} (${selectedTeamScore}) - ${opponentCompetitor.team.shortDisplayName} (${opponentScore}). `
+
+            } else {
+                let resultText = "";
+                if (selectedTeamScore > opponentScore) {
+                    resultText = `The ${selectedTeamCompetitor.team.shortDisplayName} won against the ${opponentCompetitor.team.shortDisplayName}! Final Score: `;
+                } else if (selectedTeamScore < opponentScore) {
+                    resultText = `The ${selectedTeamCompetitor.team.shortDisplayName} lost to the ${opponentCompetitor.team.shortDisplayName}! Final Score: `;
+                } else {
+                    resultText = `The ${selectedTeamCompetitor.team.shortDisplayName} tied with the ${opponentCompetitor.team.shortDisplayName}! Final Score: `;
+                }
+                gameItem.textContent = `${resultText} (${selectedTeamScore} - ${opponentScore})`;
+            }
             gameList.appendChild(gameItem);
         });
 
